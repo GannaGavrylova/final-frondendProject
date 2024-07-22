858ad0dd6549c346e8054aba166bcfff5880b076
GannaGavrylova <gannadeutsch@gmail.com>
2024-07-22 21:35:56 +0200

    working with JavaScript, added addEventListener on the Button  categoryBtn

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,33 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("footer").innerHTML = data;
     });
 });
+
+// фильтрация
+
+const urlArray =
+  "https://files.slack.com/files-pri/T05P497HJ3A-F07CZ4Q743Y/events.mock.json";
+
+const categoryBtn = document.querySelector("#categoryBtn");
+const selectCategory = document.querySelector(".category");
+
+const distanceBtn = document.querySelector("#distanceBtn");
+const selectDistance = document.querySelector(".distance");
+
+const typeBtn = document.querySelector("#typeBtn");
+
+categoryBtn.addEventListener("click", () => {
+  const ul = document.createElement("ul");
+  ul.classList.add("listContainer");
+  ul.textContent = "This is a new ul element";
+  selectCategory.append("ul");
+
+  // console.log("Element 'ul' added:", ul); // Логируем добавленный элемент
+  // console.log("Current state of selectCategory:", selectCategory.innerHTML);
+});
+
+//   Category:
+// ● Social Activities
+// ● Hobbies and Passions
+// ● Health and Wellbeing
+// ● Business
+// ● Technology
